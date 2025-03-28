35b7fb8fa2b9eb2f21f173713612077a64dc05c5
Mohanvijay8344 <Mohanvjay8344@gmail.com>
2025-03-28 09:44:41 +0530

    update

--- index.js
+++ index.js
@@ -20,6 +20,17 @@ app.get("/", (req, res) => {
   res.send("Welcome to the movie database!");
 })
 
+app.get('/all-movies', = async (req, res) => {
+  try {
+      const movies = await Movies.find();
+      res.status(200).json(movies);
+  } catch (error) {
+      console.log(`Error getting all movies controller: ${error}`);
+      res.status(500).json({ message: 'Server error' });
+      
+  }
+})
+
 app.listen(PORT, () => {
   console.log("Server running on port 8000");
   DbConnection();
