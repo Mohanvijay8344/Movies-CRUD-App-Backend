97f2c5d32551c2c1464b9e71ce38be9936679371
Mohanvijay8344 <Mohanvjay8344@gmail.com>
2025-03-28 08:32:14 +0530

    update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/", router)
+app.use("/movies", router)
 
 app.listen(PORT, () => {
   console.log("Server running on port 8000");
